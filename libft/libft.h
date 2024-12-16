37e52dcab44a60ca79bcd763b0d70dec8017ef20
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-16 11:15:19 +0100

    ft_strlen param updated

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <stdio.h>
 
-size_t ft_strlen(char *str);
+size_t ft_strlen(const char *str);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+size_t ft_strlcat(char *dest, const char *src, size_t size);
+
 #endif
