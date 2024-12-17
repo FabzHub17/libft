f8a37a269b4fe58250a85eae72ed49299acc2e05
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-17 20:28:37 +0100

    complete

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    if (*needle == '\0')
+        return ((char *)haystack);
+    while (n && *haystack)
+    {
+        if (ft_strncmp(haystack, needle, ft_strlen(needle)) == 0)
+            return ((char *)haystack);
+        haystack++;
+        n--;
+    }
+    return (NULL);
+}
+
+/*
+TEST
+
+int main(void)
+{
+    char str[] = "Hello World!";
+    char *needle = "lo";
+    char *result = ft_strnstr(str, needle, 11);
+    if (result)
+    {
+        printf("'%s' found in '%s'\n", needle, str);
+        printf("result: %s\n", result);
+    }
+    else
+        printf("'%s' not found in '%s'\n", needle, str);
+    return (0);
+}
+*/
