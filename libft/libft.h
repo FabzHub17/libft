379e13af34c60b2b2b0d47a14a812c7c2de610cc
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-17 20:28:22 +0100

    function definition added strncmp

--- libft/libft.h
+++ libft/libft.h
@@ -19,5 +19,6 @@
 size_t ft_strlen(const char *str);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 size_t ft_strlcat(char *dest, const char *src, size_t size);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
