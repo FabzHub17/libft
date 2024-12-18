5e4bcd52efc5c730f2a1db39a02e3a9e431982ae
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-18 14:52:42 +0100

    function definition added

--- libft/libft.h
+++ libft/libft.h
@@ -24,5 +24,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 void ft_bzero(void *ptr, size_t n);
 void *ft_calloc(size_t count, size_t n);
 size_t ft_strlcpy(char *dest, const char *src, size_t size);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
