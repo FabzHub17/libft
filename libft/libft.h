79f9a945725ad209ec43dad3253856e3bdf8c63f
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-18 11:34:16 +0100

    param data type corrected

--- libft/libft.h
+++ libft/libft.h
@@ -15,6 +15,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 size_t ft_strlen(const char *str);
 void *ft_memcpy(void *dest, const void *src, size_t n);
@@ -22,5 +23,6 @@ size_t ft_strlcat(char *dest, const char *src, size_t size);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void ft_bzero(void *ptr, size_t n);
 void *ft_calloc(size_t count, size_t n);
+size_t ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
