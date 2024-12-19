fa255ba3c083355f91cab6188b05e6da730f37f6
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-19 11:20:37 +0100

    complete

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,32 @@
+#include "libft.h"
+
+char *ft_strtrim(char const *s, char const *set)
+{
+    size_t start;
+    size_t end;
+    char *trimmed;
+
+    if (!s || !set)
+        return (NULL);
+    start = 0;
+    end = ft_strlen(s) - 1; // l'indice dell'ultimo carattere.
+    while (start <= end && ft_strchr(set, s[start]))
+        start++;
+    while (end >= start && ft_strchr(set, s[end]))
+        end--;
+    trimmed = ft_substr(s, start, end - start + 1); //  +1 per includere il terminator.
+    return (trimmed);
+}
+
+/*
+TEST
+int main(void)
+{
+    char *str = "   Hello, World!   ";
+    char *set = "H";
+    char *trimmed = ft_strtrim(str, set);
+    printf("Trimmed string: %s\n", trimmed);
+    free(trimmed);
+    return (0);
+}
+*/
