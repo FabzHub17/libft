8d8a5181ed3f682238e79d484a69b3f1e3994ad8
Thashiru Chandupa Fabio Vitharana <tvithara@e4r5p3.42roma.it>
2024-12-21 18:05:56 +0100

    function definitions added

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 size_t ft_strlen(const char *str);
 void *ft_memcpy(void *dest, const void *src, size_t n);
@@ -27,5 +28,9 @@ size_t ft_strlcpy(char *dest, const char *src, size_t size);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strchr(const char *s, int c);
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+
 
 #endif
