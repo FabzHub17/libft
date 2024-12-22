06c10b4083a6d79256f0c768e763fc4795dd197a
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2024-12-22 11:51:41 +0100

    updated

--- libft/libft.h
+++ libft/libft.h
@@ -32,5 +32,20 @@ void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
 
+// BONUS CONTENT
+typedef struct s_list
+{
+    void *content;
+    struct s_list *next;
+} t_list;
+
+t_list *ft_lstnew(void *content);
+void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void *));
+void ft_lstclear(t_list **lst, void (*del)(void *));
+void ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
